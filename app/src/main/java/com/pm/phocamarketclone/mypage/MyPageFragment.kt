228f694302f2865fe7349fbfdddcd46d0a37576f
Charlie_Moon <moon3482@cau.ac.kr>
2022-06-26 22:56:59 +0900

    마이페이지 프레그먼트 생성

---
+++ app/src/main/java/com/pm/phocamarketclone/mypage/MyPageFragment.kt
@@ -0,0 +1,33 @@
+package com.pm.phocamarketclone.mypage
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MyPageFragment:Fragment(){
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
