38e2b23f4eabe43d5b43daa218b7dc92765409cb
Charlie_Moon <moon3482@cau.ac.kr>
2022-06-27 21:53:43 +0900

    없는 카드 제보하기 레이아웃 생성 데이터 바인딩 추가 리사이클러뷰 아이템 추가

---
+++ app/src/main/java/com/pm/phocamarketclone/mypage/PhotoCardReportFragment.kt
@@ -0,0 +1,37 @@
+package com.pm.phocamarketclone.mypage
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pm.phocamarketclone.databinding.FragmentPhotocardReportBinding
+
+
+class PhotoCardReportFragment : Fragment() {
+
+    private val binding by lazy { FragmentPhotocardReportBinding.inflate(layoutInflater) }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
