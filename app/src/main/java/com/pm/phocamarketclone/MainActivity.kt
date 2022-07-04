547760deec821111b0716190294181ba53a48837
Charlie_Moon <moon3482@cau.ac.kr>
2022-07-04 23:38:54 +0900

    메인 액티비티 프래그먼트, 네비게이션 기초 작업

--- app/src/main/java/com/pm/phocamarketclone/MainActivity.kt
+++ app/src/main/java/com/pm/phocamarketclone/MainActivity.kt
@@ -2,10 +2,37 @@ package com.pm.phocamarketclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.pm.phocamarketclone.databinding.ActivityMainBinding
+import com.pm.phocamarketclone.mypage.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val myPageFragment = MyPageFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        viewInit()
+
+    }
+
+    private fun viewInit(){
+        supportFragmentManager.beginTransaction().add(R.id.fl_fragment, TODO("need SearchFragment")).commit()
+
+        binding.bnavMain.setOnItemSelectedListener {
+            when (it.itemId) {
+//                TODO("need replace SearchFragment")->{}
+                R.id.nav_my_page -> {
+                    replaceFragment(myPageFragment)
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fl_fragment, fragment).commit()
     }
 }
