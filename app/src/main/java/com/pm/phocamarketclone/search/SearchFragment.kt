a778cdd952a7107683dd41166c86a93b933ebbae
johs <joker9302@gmail.com>
2022-07-05 10:38:21 +0900

    SearchFragment.kt 포카찾기 메뉴 클릭 시 프래그먼트 보이게 수정

--- app/src/main/java/com/pm/phocamarketclone/search/SearchFragment.kt
+++ app/src/main/java/com/pm/phocamarketclone/search/SearchFragment.kt
@@ -1,6 +1,18 @@
 package com.pm.phocamarketclone.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.pm.phocamarketclone.R
+import com.pm.phocamarketclone.databinding.FragmentSearchBinding
 
 class SearchFragment: Fragment() {
+    lateinit var binding : FragmentSearchBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+        return binding.root
+    }
 }
