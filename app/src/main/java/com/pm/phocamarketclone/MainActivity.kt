ad9c956625c5bbfeb21792b293ee792a22b38ab1
johs <joker9302@gmail.com>
2022-07-05 09:45:00 +0900

    MainActivity.kt viewinit 관련 수정

--- app/src/main/java/com/pm/phocamarketclone/MainActivity.kt
+++ app/src/main/java/com/pm/phocamarketclone/MainActivity.kt
@@ -1,32 +1,33 @@
 package com.pm.phocamarketclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.pm.phocamarketclone.databinding.ActivityMainBinding
 import com.pm.phocamarketclone.mypage.MyPageFragment
+import com.pm.phocamarketclone.phocatalk.PhocaTalkFragment
+import com.pm.phocamarketclone.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val searchFragment = SearchFragment()
+    private val talkFragment = PhocaTalkFragment()
     private val myPageFragment = MyPageFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         viewInit()
 
     }
 
-    private fun viewInit(){
-        supportFragmentManager.beginTransaction().add(R.id.fl_fragment, TODO("need SearchFragment")).commit()
-
+    private fun viewInit() {
+        supportFragmentManager.beginTransaction().add(R.id.fl_fragment, searchFragment).commit()
         binding.bnavMain.setOnItemSelectedListener {
             when (it.itemId) {
-//                TODO("need replace SearchFragment")->{}
-                R.id.nav_my_page -> {
-                    replaceFragment(myPageFragment)
-                }
+                R.id.nav_phoca_find -> replaceFragment(searchFragment)
+                R.id.nav_phoca_talk -> replaceFragment(talkFragment)
+                R.id.nav_my_page -> replaceFragment(myPageFragment)
             }
             true
         }
