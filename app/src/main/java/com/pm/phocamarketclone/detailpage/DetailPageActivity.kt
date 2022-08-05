0ded9db410ef9e96575f31843a36b3b20274c523
johs <joker9302@gmail.com>
2022-08-05 15:12:35 +0900

    상세페이지 매칭 대기중인 포카 더미데이터 연동

--- app/src/main/java/com/pm/phocamarketclone/detailpage/DetailPageActivity.kt
+++ app/src/main/java/com/pm/phocamarketclone/detailpage/DetailPageActivity.kt
@@ -3,12 +3,34 @@ package com.pm.phocamarketclone.detailpage
 import com.pm.phocamarketclone.R
 import com.pm.phocamarketclone.base.BaseViewModelActivity
 import com.pm.phocamarketclone.databinding.ActivityDetailPageBinding
+import com.pm.phocamarketclone.detailpage.recyclerview.DetailListAdapter
 
 class DetailPageActivity :
     BaseViewModelActivity<ActivityDetailPageBinding, DetailPageActivityViewModel>(
         R.layout.activity_detail_page,
         DetailPageActivityViewModel::class.java
     ) {
+    private val detailListAdapter = DetailListAdapter()
 
+    override fun onInitBinding() {
+        super.onInitBinding()
+        binding.apply {
+            vm = viewModel
+            lifecycleOwner = this@DetailPageActivity
+            rvBuyOrSaleInfo.run {
+                adapter = detailListAdapter
+                itemAnimator = null
+            }
+        }
+    }
 
+    override fun observeChanges() {
+        super.observeChanges()
+        viewModel.buyOrSaleList.observe(this){
+            detailListAdapter.submitList(it)
+        }
+        viewModel.isStateBuyOrSale.observe(this){
+            viewModel.getBuyOrSaleList(it)
+        }
+    }
 }
--- app/src/main/java/com/pm/phocamarketclone/detailpage/DetailPageActivityViewModel.kt
+++ app/src/main/java/com/pm/phocamarketclone/detailpage/DetailPageActivityViewModel.kt
@@ -1,6 +1,49 @@
 package com.pm.phocamarketclone.detailpage
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.pm.phocamarketclone.detailpage.data.DetailData
 
-class DetailPageActivityViewModel:ViewModel() {
+enum class DetailState {
+    BUY, SALE
+}
+
+class DetailPageActivityViewModel : ViewModel() {
+    private val _buyOrSaleList = MutableLiveData<List<DetailData>>()
+    val buyOrSaleList: LiveData<List<DetailData>> = _buyOrSaleList
+    var isStateBuyOrSale = MutableLiveData<DetailState>()
+
+    init {
+        getBuyOrSaleList(DetailState.BUY)
+        isStateBuyOrSale.value = DetailState.BUY
+    }
+
+    fun getBuyOrSaleList(type: DetailState) {
+        val buyData = listOf(
+            DetailData(hopePrice = 3000L),
+            DetailData(hopePrice = 2000L),
+            DetailData(hopePrice = 1000L),
+            DetailData(hopePrice = 3500L),
+            DetailData(hopePrice = 7000L)
+        )
+        val saleData = listOf(
+            DetailData(hopePrice = 1000L),
+            DetailData(hopePrice = 2000L),
+            DetailData(hopePrice = 3000L),
+            DetailData(hopePrice = 4500L),
+            DetailData(hopePrice = 5000L)
+        )
+        _buyOrSaleList.value = when (type) {
+            DetailState.BUY -> buyData
+            DetailState.SALE -> saleData
+        }
+    }
+
+    fun setOnClickMatchWaitingState(type: DetailState) {
+        when (type) {
+            DetailState.BUY -> isStateBuyOrSale.value = DetailState.BUY
+            DetailState.SALE -> isStateBuyOrSale.value = DetailState.SALE
+        }
+    }
 }
