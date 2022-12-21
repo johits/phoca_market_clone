3eafad4b441454fc96d5c8bd98f987ae6df22a07
jhs-torder <jhs2@torder.co.kr>
2022-12-21 15:23:32 +0900

    FCM test

--- presentation/src/main/java/com/pm/presentation/MainActivity.kt
+++ presentation/src/main/java/com/pm/presentation/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.pm.presentation
 
+import android.annotation.TargetApi
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
 import com.pm.presentation.databinding.ActivityMainBinding
 import com.pm.presentation.mypage.MyPageFragment
 import com.pm.presentation.phocatalk.PhocaTalkFragment
@@ -23,12 +35,57 @@ class MainActivity : AppCompatActivity() {
     private val talkFragment = PhocaTalkFragment()
     private val myPageFragment = MyPageFragment()
     val db = FirebaseFirestore.getInstance()
+
+    private val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewInit()
-        val fcm = Intent(applicationContext, MyFirebaseMessaging::class.java)
-        startService(fcm)
+        checkPermission()
+        Log.d("jhs", "인터넷 연결 체크 상태:${isConnectInternet()}")
+
+        //서비스 시작
+        var intent = Intent(this, MyFirebaseMessaging::class.java)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            this.startForegroundService(intent)
+        } else {
+            this.startService(intent)
+        }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("jhs", "getInstanceId failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new Instance ID token
+            val token = task.result
+
+            Log.d("jhs", "토큰:$token")
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        processcommand(intent)
+    }
+
+    private fun processcommand(intent: Intent) {
+        if (intent != null) {
+            if (intent?.action == "test") {
+                val builder = AlertDialog.Builder(this@MainActivity)
+                builder.apply {
+                    setTitle(intent.getStringExtra("title"))
+                    setMessage(intent.getStringExtra("body"))
+                    setPositiveButton("확인", null)
+                    show()
+                }
+            }
+        }
     }
 
     private fun viewInit() {
@@ -41,6 +98,13 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
+
+    private fun isConnectInternet(): String { // 인터넷 연결 체크 함수
+        val cm: ConnectivityManager =
+            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = cm.activeNetworkInfo
+        return networkInfo.toString()
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -71,5 +135,39 @@ class MainActivity : AppCompatActivity() {
                     e
                 )
             })
+    }
+
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
+            if (!Settings.canDrawOverlays(this)) {              // 다른앱 위에 그리기 체크
+                val uri: Uri = Uri.fromParts("package", packageName, null)
+                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, uri)
+                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE)
+            } else {
+                startMain()
+            }
+        } else {
+            startMain()
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                finish()
+            } else {
+                startMain()
+            }
+        }
+    }
+
+    private fun startMain() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(Intent(this, MyFirebaseMessaging::class.java))
+        } else {
+            startService(Intent(this, MyFirebaseMessaging::class.java))
+        }
     }
 }
--- presentation/src/main/java/com/pm/presentation/MyFirebaseMessaging.kt
+++ presentation/src/main/java/com/pm/presentation/MyFirebaseMessaging.kt
@@ -1,14 +1,178 @@
 package com.pm.presentation
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.os.PowerManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import timber.log.Timber
+
 
 class MyFirebaseMessaging : FirebaseMessagingService() {
-    override fun onMessageReceived(p0: RemoteMessage) {
-        super.onMessageReceived(p0)
+
+    override fun onCreate() {
+        super.onCreate()
+        val CHANNEL_ID = "channel_1"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Timber.w("jhs channel")
+            val channel = NotificationChannel(
+                CHANNEL_ID, "Android test",
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+            (getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("")
+            .setContentText("").build()
+        startForeground(2, notification)
     }
 
-    override fun onNewToken(p0: String) {
-        super.onNewToken(p0)
+    /** 푸시 알림으로 보낼 수 있는 메세지는 2가지
+     * 1. Notification: 앱이 실행중(포그라운드)일 떄만 푸시 알림이 옴
+     * 2. Data: 실행중이거나 백그라운드(앱이 실행중이지 않을때) 알림이 옴 -> TODO: 대부분 사용하는 방식 */
+
+    private val TAG = "FirebaseService"
+
+    /** Token 생성 메서드(FirebaseInstanceIdService 사라짐) */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "new Token: $token")
+
+        // 토큰 값을 따로 저장
+        val pref = this.getSharedPreferences("token", Context.MODE_PRIVATE)
+        val editor = pref.edit()
+        editor.putString("token", token).apply()
+        editor.commit()
+        Log.i(TAG, "성공적으로 토큰을 저장함")
+    }
+
+
+    /** 메시지 수신 메서드(포그라운드) */
+    @SuppressLint("InvalidWakeLockTag")
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.i("SellerFirebaseService ", "Message :: $remoteMessage")
+
+        //화면 깨우기
+        var powerManager = getSystemService(POWER_SERVICE) as PowerManager
+        val wakeLock = powerManager.newWakeLock(
+            PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.ON_AFTER_RELEASE,
+            "WAKELOCK"
+        )
+        //수신 메시지
+        val data = remoteMessage.data
+        val title = data["title"]
+        val body = data["body"]
+
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.addFlags(
+            Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP or
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
+        )
+        intent.action = "test"
+        intent.putExtra("title", title)
+        intent.putExtra("body", body)
+        startActivity(intent)
+
+        Log.i("jhs ", "바디 : $body // 타이틀:$title")
+        wakeLock.acquire(1 * 60 * 1000L /*1 minutes*/); // WakeLock 깨우기
+        sendNotification(remoteMessage)
+    }
+
+
+    /** 알림 생성 메서드 */
+    private fun sendNotification(remoteMessage: RemoteMessage) {
+        // RequestCode, Id를 고유값으로 지정하여 알림이 개별 표시
+        val uniId: Int = (System.currentTimeMillis() / 7).toInt()
+
+        // 일회용 PendingIntent : Intent 의 실행 권한을 외부의 어플리케이션에게 위임
+        val intent = Intent(this, MainActivity::class.java)
+        //각 key, value 추가
+        for (key in remoteMessage.data.keys) {
+            intent.putExtra(key, remoteMessage.data.getValue(key))
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // Activity Stack 을 경로만 남김(A-B-C-D-B => A-B)
+        val pendingIntent =
+            PendingIntent.getActivity(this, uniId, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        // 알림 채널 이름
+        val channelId = "my_channel"
+        // 알림 소리
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        // 알림에 대한 UI 정보, 작업
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_send) // 아이콘 설정
+            .setContentTitle(remoteMessage.data["title"].toString()) // 제목
+            .setContentText(remoteMessage.data["body"].toString()) // 메시지 내용
+            .setAutoCancel(true) // 알람클릭시 삭제여부
+            .setSound(soundUri)  // 알림 소리
+            .setContentIntent(pendingIntent) // 알림 실행 시 Intent
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // 오레오 버전 이후에는 채널이 필요
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // 알림 생성
+        notificationManager.notify(uniId, notificationBuilder.build())
+    }
+
+    /** Token 가져오기 */
+    fun getFirebaseToken() {
+        //비동기 방식
+        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            Log.d(TAG, "token=${it}")
+        }
+
+//		  //동기방식
+//        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+//                if (!task.isSuccessful) {
+//                    Log.d(TAG, "Fetching FCM registration token failed ${task.exception}")
+//                    return@OnCompleteListener
+//                }
+//                var deviceToken = task.result
+//                Log.e(TAG, "token=${deviceToken}")
+//            })
     }
 }
+
+/**
+"notificationBuilder" 알림 생성시 여러가지 옵션을 이용해 커스텀 가능.
+setSmallIcon : 작은 아이콘 (필수)
+setContentTitle : 제목 (필수)
+setContentText : 내용 (필수)
+setColor : 알림내 앱 이름 색
+setWhen : 받은 시간 커스텀 ( 기본 시스템에서 제공합니다 )
+setShowWhen : 알림 수신 시간 ( default 값은 true, false시 숨길 수 있습니다 )
+setOnlyAlertOnce : 알림 1회 수신 ( 동일 아이디의 알림을 처음 받았을때만 알린다, 상태바에 알림이 잔존하면 무음 )
+setContentTitle : 제목
+setContentText : 내용
+setFullScreenIntent : 긴급 알림 ( 자세한 설명은 아래에서 설명합니다 )
+setTimeoutAfter : 알림 자동 사라지기 ( 지정한 시간 후 수신된 알림이 사라집니다 )
+setContentIntent : 알림 클릭시 이벤트 ( 지정하지 않으면 클릭했을때 아무 반응이 없고 setAutoCancel 또한 작동하지 않는다 )
+setLargeIcon : 큰 아이콘 ( mipmap 에 있는 아이콘이 아닌 drawable 폴더에 있는 아이콘을 사용해야 합니다. )
+setAutoCancel : 알림 클릭시 삭제 여부 ( true = 클릭시 삭제 , false = 클릭시 미삭제 )
+setPriority : 알림의 중요도를 설정 ( 중요도에 따라 head up 알림으로 설정할 수 있는데 자세한 내용은 밑에서 설명하겠습니다. )
+setVisibility : 잠금 화면내 알림 노출 여부
+Notification.VISIBILITY_PRIVATE : 알림의 기본 정보만 노출 (제목, 타이틀 등등)
+Notification.VISIBILITY_PUBLIC : 알림의 모든 정보 노출
+Notification.VISIBILITY_SECRET : 알림의 모든 정보 비노출
+ */
